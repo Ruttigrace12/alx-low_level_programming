97b5fb3e91fd9234b79ae7992ef0a2692a079791
Ruttigrace12 <rutty5683@gmail.com>
2022-09-20 08:59:47 -0700

    putss

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * puts2 - print one char out of 2 of a string
  * @str: char array string type
  * Description: Print 1st char, then 3rd, then 5th, etc..
  */
-
 void puts2(char *str)
 {
 	int i;
@@ -16,4 +14,4 @@ void puts2(char *str)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
-}i          
+}
